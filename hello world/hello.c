d97ccaae1cc3f610c976689b8195bedca69ae7cb
mahilion <mahilion01@gmail.com>
2015-11-10 19:06:11 +0530

    nov 10 time6:43pm

---
+++ hello world/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//include standard input and output header files
+int main(void)
+{
+printf("welcome to c programming\n");
+//print the string on to screen
+return 0;
+}
---
+++ sum of two no/sum2no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+int a ,b;
+printf("enter the value for a : ");
+scanf("%d",&a);
+printf("\nenter the value for b : ");
+scanf("%d",&b);
+printf("\nthe sum of %d and %d is %d ",a,b,a+b);
+printf("\ndone");
+return 0;
+
+}
